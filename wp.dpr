331806a7721d4e1eaf86a3323dc9bf3153b08b21
Xhersi Lula <xhersi.lula@gmail.com>
2016-09-03 11:09:42 +0200

    download 10e pare wps per page

--- wp.dpr
+++ wp.dpr
@@ -1,30 +1,27 @@
 program wp;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
-  System.SysUtils, RegularExpressions,
-  HTTPApp,
-  IdHTTP,
-  XMLDoc,
-  XMLIntf,
-  ActiveX, System.Classes, System.StrUtils;
+  System.SysUtils, RegularExpressions, System.Types, IdHTTP, UrlMon,
+  ActiveX, System.StrUtils, System.Classes;
 
 const
   RES = '1680x1050';
-  UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2837.0 Safari/537.36';
+  // UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2837.0 Safari/537.36';
 
 var
-  CoResult: Integer;
+  CoResult, i: Integer;
   HTTP: TIdHTTP;
   Query: String;
-  ResultString: String;
+  regexpr: TRegEx;
+  match: TMatch;
+  token: TStringDynArray;
 
-function Occurrences(const Substring, Text: string): integer;
+function Occurrences(const Substring, Text: string): Integer;
 var
-  offset: integer;
+  offset: Integer;
 begin
   result := 0;
   offset := PosEx(Substring, Text, 1);
@@ -35,14 +32,16 @@ begin
   end;
 end;
 
-function wallpaper_get_url(AWp_id, ABase, ARes: string) : String;
+function wallpaper_get_url(AWp_id, ABase, ARes: string): String;
 begin
-  Result := Format('http://interfacelift.com/wallpaper/7yz4ma1/%s_%s_%s.jpg', [AWp_id, ABase, ARes]) ;
+  result := Format('http://interfacelift.com/wallpaper/7yz4ma1/%s_%s_%s.jpg',
+    [AWp_id, ABase, ARes]);
 end;
 
-function grab_page(AIndex: Integer) : String;
+function grab_page(AIndex: Integer): String;
 begin
-  Query := Format('https://interfacelift.com/wallpaper/downloads/date/any/index%d.html', [AIndex]);
+  Query := Format
+    ('https://interfacelift.com/wallpaper/downloads/date/any/index%d.html', [AIndex]);
 
   CoResult := CoInitializeEx(nil, COINIT_MULTITHREADED);
 
@@ -54,15 +53,42 @@ begin
 
   HTTP := TIdHTTP.Create;
 
-  Result := HTTP.Get(Query);
+  result := HTTP.Get(Query);
 end;
 
+procedure download_file(AUrl: String);
+var
+  Path: String;
+  FileName: String;
+  List: TStringDynArray;
+  IdHTTP1: TIdHTTP;
+  Stream: TMemoryStream;
+begin
+  Path := (ExtractFilePath(ParamStr(0)) + 'downloads/');
+
+  if not directoryexists(Path) then
+    CreateDir(Path);
+
+  List := SplitString(AUrl, '/');
+  FileName := List[length(List) - 1];
+
+  URLDownloadToFile(nil, Pchar(AUrl), Pchar(Path + FileName), 0, nil);
+end;
 
 begin
   try
-    { TODO -oUser -cConsole Main : Insert code here }
-    ResultString := TRegEx.Match( grab_page(1), 'imgload\(''(.+)?'', this,''(.+)''\)').Value;
-    Writeln(ResultString);
+    regexpr := TRegEx.Create('imgload\(''(.+)?'', this,''(.+)''\)', [roIgnoreCase, roMultiline]);
+    match := regexpr.match(grab_page(1));
+
+    i := 1;
+    while match.Success do
+    begin
+      token := SplitString(match.Value, '''');
+      download_file(wallpaper_get_url(token[3], token[1], RES));
+      Writeln(format('%d of %d downloaded', [i, 10]));
+      match := match.NextMatch;
+      Inc(i, 1);
+    end;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
